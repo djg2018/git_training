2beafee2902b9575c7f0feab3fefa8391ce5439c
djg2018 <43592724+djg2018@users.noreply.github.com>
2025-01-23 19:43:50 +0530

    Create main.tf
    Initial Commit

---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region = var.region
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.25"
+  subnets         = module.vpc.public_subnets
+  vpc_id          = module.vpc.vpc_id
+  node_groups = {
+    eks_nodes = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_type    = "t3.medium"
+    }
+  }
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "eks-vpc"
+  cidr    = "10.0.0.0/16"
+  azs     = ["us-east-1a", "us-east-1b"]
+  public_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
